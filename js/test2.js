5868dd3a872c2fdeb7f961063deb90b0e1db050d
Igor <artvid29@gmail.com>
2019-05-08 10:54:56 +0300

    Комментарии

--- js/test2.js
+++ js/test2.js
@@ -1,24 +1,27 @@
 //№2 FizzBuzz
-// for(let i=0;i<100;i++)
-// {
-//     if (i%3==0)
-//     {console.log("Fizz");}
-//     else if(i%5==0&&i%3!=0){
-//         console.log("Buzz")
-//     }
-//     else
-//     {console.log(i);}
+// Для чисел,нацело делящихся на 3, она должна выводить ‘Fizz’, а
+//для чисел, делящихся на 5 (но не на 3) – ‘Buzz’.
+for(let i=0;i<100;i++)
+{
+    if (i%3==0)
+    {console.log("Fizz");}
+    else if(i%5==0&&i%3!=0){
+        console.log("Buzz")
+    }
+    else
+    {console.log(i);}
    
-// }
+}
 
 
-
-// for(let i=0;i<100;i++)
-// {
-//     if ((i%3==0)&&(i%5==0))
-//     {console.log("FizzBuzz");}
+//исправьте её так, чтобы она выводила
+//«FizzBuzz» для всех чисел, которые делятся и на 3, и на 5.
+for(let i=0;i<100;i++)
+{
+    if ((i%3==0)&&(i%5==0))
+    {console.log("FizzBuzz");}
        
-//     else
-//     {console.log(i);}
+    else
+    {console.log(i);}
    
-// }
+}
