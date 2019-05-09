68d085533573696d3f4b2e0ef73aef716e46275b
Igor <artvid29@gmail.com>
2019-05-09 08:15:12 +0300

    доработана №5 про range

--- js/test5.js
+++ js/test5.js
@@ -1,35 +1,66 @@
 //№5 Про range
-
-
 function newArray()
 {
     let n= +prompt("Введите начало диапазона n?","");//левая граница массива
     let m= +prompt("Введите конец диапазона m?","");//правая граница массива
     let testArray=[];// объявление пустого массива, подлежащего заполнению
-    let j=0;//объявление начального индекса массива
-
-for(i=n;i<=m;i++)//цикл заполнения массива
-    {
-    testArray[j]=i;
-    j++;
+    
+if(m>n){
+for(i=n;i<=m;i++){//цикл заполнения массива
+    testArray.push(i);
     }
+}
+else{
+for(i=n;i>=m;i--){
+    testArray.push(i);
+    }
+}
 return testArray; //возвращаем массив
 }
 
-g=newArray();
-console.log(g);
-alert(g);
-
+let resultArray=newArray();
+alert(resultArray);
 
 //Подсчитываем сумму полученного массива
-function sum(task){
-    let sum=0;
-    for(i=0;i<task.length;i++)
-    {
-        sum=sum+task[i];
-    }
-    return sum;
-}
 
-alert(sum(g));
+let sumOfArray=resultArray.reduce(function(accum,currentValue){return accum+currentValue;});
+alert(sumOfArray);
+
+
+
+
+
+
+
+// function newArray()
+// {
+//     let n= +prompt("Введите начало диапазона n?","");//левая граница массива
+//     let m= +prompt("Введите конец диапазона m?","");//правая граница массива
+//     let testArray=[];// объявление пустого массива, подлежащего заполнению
+//     let j=0;//объявление начального индекса массива
+
+// for(i=n;i<=m;i++)//цикл заполнения массива
+//     {
+//     testArray[j]=i;
+//     j++;
+//     }
+// return testArray; //возвращаем массив
+// }
+
+// g=newArray();
+// console.log(g);
+// alert(g);
+
+
+// //Подсчитываем сумму полученного массива
+// function sum(task){
+//     let sum=0;
+//     for(i=0;i<task.length;i++)
+//     {
+//         sum=sum+task[i];
+//     }
+//     return sum;
+// }
+
+// alert(sum(g));
 
